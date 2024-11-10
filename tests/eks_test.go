bd7595b5961e66f0fa853f3c1b2902a3e2f6a068
nathancarrilho <nathancarrilholeal@gmail.com>
2024-11-10 12:55:32 +0000

    fix: adding pipeline

---
+++ tests/eks_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestEKSExample(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+}
